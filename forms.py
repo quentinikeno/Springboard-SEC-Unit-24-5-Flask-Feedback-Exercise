bfd13a1e4dfe5ca916155d7c3e12144ddef4a884
Quentin Ikeno <ikenoq@gmail.com>
2022-03-19 07:59:48 -1000

    Create form to register users

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import InputRequired, Email
+
+class UserRegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    email = EmailField("Email", validators=[InputRequired(), Email()])
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()])
