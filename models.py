fdd9c2f864adf95cf562fe5df516f75c7085ef7f
Quentin Ikeno <ikenoq@gmail.com>
2022-03-19 12:29:27 -1000

    Add method for authentication

--- models.py
+++ models.py
@@ -39,3 +39,16 @@ class User(db.Model):
         # turn bytestring into normal (unicode utf8) string
         hashed_utf8 = hashed.decode("utf8")
         return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+    
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """
+        user = User.query.filter_by(username=username).first()
+        
+        if user and bcrypt.check_password_hash(user.password, pwd):
+            return user
+        else:
+            return False
