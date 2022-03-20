a8c015ae0352b091c0cc3ed2248978f8c718a25e
Quentin Ikeno <ikenoq@gmail.com>
2022-03-20 12:11:49 -1000

    Create Feedback model

--- models.py
+++ models.py
@@ -56,3 +56,17 @@ class User(db.Model):
             return user
         else:
             return False
+        
+class Feedback(db.Model):
+    
+    __tablename__ = "feedback"
+    
+    id = db.Column(db.Integer, primary_key=True, auto_increment = True)
+    
+    title = db.Column(db.String(100), nullable=False)
+    
+    content = db.Column(db.Text, nullable=False)
+    
+    username = db.Column(db.String(20), db.ForeignKey("users.username"))
+    
+    user = db.relationship("User", backref="feedback")
